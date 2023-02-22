d737ff872a5a35e81fe8d192d161fda203f6ffe4
george-hall-ucl <george.hall@ucl.ac.uk>
2023-02-22 17:17:00 +0000

    Add tests for generate_neighbor_labels()

---
+++ tests/testthat/test_generate_neighbor_labels.R
@@ -0,0 +1,37 @@
+library(Seurat)
+
+test_that("generate_neighbor_labels", {
+    cells <- readRDS("../../data/five_cell_seurat.RDS")
+    # cells$label is c("Condition1", "Condition2", "Condition1",
+    #                  "Condition2", "Condition1")
+
+    set.seed(123)
+    mat <- cbind(1:5, t(apply(t(matrix(rep(1:5, 5), ncol = 5)), 1, sample)))
+    #      [,1] [,2] [,3] [,4] [,5] [,6]
+    # [1,]    1    3    2    5    4    1
+    # [2,]    2    3    1    2    5    4
+    # [3,]    3    2    3    1    4    5
+    # [4,]    4    1    4    5    3    2
+    # [5,]    5    3    4    2    1    5 
+    cells@neighbors$RNA.nn@nn.idx <- mat
+
+    actual <- generate_neighbor_labels(cells, reduced_dim = "foo",
+                                       recalculate_graph = FALSE,
+                                       label_names = "label", verbose = FALSE)
+    # Should be:
+    #      [,1] [,2] [,3] [,4] [,5]
+    # [1,]    1    0    1    0    1
+    # [2,]    1    1    0    1    0
+    # [3,]    0    1    1    0    1
+    # [4,]    1    0    1    1    0
+    # [5,]    1    0    0    1    1
+
+    expected <- t(matrix(c(1, 0, 1, 0, 1,
+                           1, 1, 0, 1, 0,
+                           0, 1, 1, 0, 1,
+                           1, 0, 1, 1, 0,
+                           1, 0, 0, 1, 1),
+                         ncol = 5))
+
+    expect_equal(actual, expected)
+})

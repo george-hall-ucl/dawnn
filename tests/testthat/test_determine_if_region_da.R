470b23f7e67b846f21aed3217f4e7c9fff327f5b
george-hall-ucl <george.hall@ucl.ac.uk>
2023-02-27 22:03:20 +0000

    Remove assume_independence from tests

--- tests/testthat/test_determine_if_region_da.R
+++ tests/testthat/test_determine_if_region_da.R
@@ -6,11 +6,9 @@ test_that("determine_if_region_da reproducible", {
     n <- rbeta(n = 1000, shape1 = 50, shape2 = 50)
     p <- rbeta(n = 1000, shape1 = 0.7, shape2 = 0.4)
     verdict_1 <- determine_if_region_da(p_vals = p, scores = s,
-                                        null_dist = n, alpha = 0.1,
-                                        assume_independence = TRUE)
+                                        null_dist = n, alpha = 0.1)
     verdict_2 <- determine_if_region_da(p_vals = p, scores = s,
-                                        null_dist = n, alpha = 0.1,
-                                        assume_independence = TRUE)
+                                        null_dist = n, alpha = 0.1)
     expect_equal(verdict_1, verdict_2)
 })
 
@@ -20,7 +18,6 @@ test_that("determine_if_region_da returns vector", {
     n <- rbeta(n = 1000, shape1 = 50, shape2 = 50)
     p <- rbeta(n = 1000, shape1 = 0.7, shape2 = 0.4)
     verdict <- determine_if_region_da(p_vals = p, scores = s,
-                                      null_dist = n, alpha = 0.1,
-                                      assume_independence = TRUE)
+                                      null_dist = n, alpha = 0.1)
     expect_vector(verdict, ptype = logical(), size = 1000)
 })

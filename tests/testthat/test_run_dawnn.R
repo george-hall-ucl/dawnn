2fc1bb27f580c052903041275466776e774816b0
george-hall-ucl <george.hall@ucl.ac.uk>
2023-03-24 15:01:29 +0000

    Test 'run_dawnn reproducible recalculate_graph = TRUE' now passes

--- tests/testthat/test_run_dawnn.R
+++ tests/testthat/test_run_dawnn.R
@@ -1,5 +1,3 @@
-# This test currently passes, however:
-# * It crashes if I set recalculate_graph to TRUE
 test_that("run_dawnn reproducible recalculate_graph = FALSE", {
     cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
     cells <- FindNeighbors(cells, reduction = "pca", k.param = 1001,
@@ -20,26 +18,26 @@ test_that("run_dawnn reproducible recalculate_graph = FALSE", {
     expect_equal(dawnn_out_1, dawnn_out_2)
 })
 
-# test_that("run_dawnn reproducible recalculate_graph = TRUE", {
-#     cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
-#     cells <- FindNeighbors(cells, reduction = "pca", k.param = 1001,
-#                            dims = 1:2, return.neighbor = TRUE)
-# 
-#     model_file <- "~/.dawnn/dawnn_nn_model.h5"
-#     dawnn_out_1 <- sm(run_dawnn(cells = cells, label_names = "label",
-#                                 label_1 = "Condition1", label_2 = "Condition2",
-#                                 nn_model = model_file, reduced_dim = "pca",
-#                                 recalculate_graph = TRUE, alpha = 0.1,
-#                                 verbosity = 0))
-# 
-#     dawnn_out_2 <- sm(run_dawnn(cells = cells, label_names = "label",
-#                                 label_1 = "Condition1", label_2 = "Condition2",
-#                                 nn_model = model_file, reduced_dim = "pca",
-#                                 recalculate_graph = TRUE, alpha = 0.1,
-#                                 verbosity = 0))
-# 
-#     expect_equal(dawnn_out_1, dawnn_out_2)
-# })
+test_that("run_dawnn reproducible recalculate_graph = TRUE", {
+    cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
+    cells <- FindNeighbors(cells, reduction = "pca", k.param = 1001,
+                           dims = 1:2, return.neighbor = TRUE)
+
+    model_file <- "~/.dawnn/dawnn_nn_model.h5"
+    dawnn_out_1 <- sm(run_dawnn(cells = cells, label_names = "label",
+                                label_1 = "Condition1", label_2 = "Condition2",
+                                nn_model = model_file, reduced_dim = "pca",
+                                n_dims = 2, recalculate_graph = TRUE,
+                                alpha = 0.1, verbosity = 0))
+
+    dawnn_out_2 <- sm(run_dawnn(cells = cells, label_names = "label",
+                                label_1 = "Condition1", label_2 = "Condition2",
+                                nn_model = model_file, reduced_dim = "pca",
+                                n_dims = 2, recalculate_graph = TRUE,
+                                alpha = 0.1, verbosity = 0))
+
+    expect_equal(dawnn_out_1, dawnn_out_2)
+})
 
 test_that("run_dawnn returns Seurat", {
     cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")

18aa73d3bd49d09419e5f656e8630034a3be2f57
george-hall-ucl <george.hall@ucl.ac.uk>
2023-05-09 10:04:03 +0100

    Use less specific test for model class

--- tests/testthat/test_load_model_from_python.R
+++ tests/testthat/test_load_model_from_python.R
@@ -5,17 +5,11 @@ test_that("Incorrect model_path leads to error", {
 
 test_that("Loaded model has the correct class", {
               model <- load_model_from_python("~/.dawnn/dawnn_nn_model.h5")
-              model_class <- class(model)
+              model_class <- class(model)[1:4]
               goal_class_values <- c("keras.engine.sequential.Sequential",
                                      "keras.engine.functional.Functional",
                                      "keras.engine.training.Model",
-                                     "keras.engine.base_layer.Layer",
-                                     "tensorflow.python.module.module.Module",
-                                     "tensorflow.python.training.tracking.autotrackable.AutoTrackable",
-                                     "tensorflow.python.training.tracking.base.Trackable",
-                                     "keras.utils.version_utils.LayerVersionSelector",
-                                     "keras.utils.version_utils.ModelVersionSelector",
-                                     "python.builtin.object")
+                                     "keras.engine.base_layer.Layer")
               expect_equal(model_class, goal_class_values)
 })
 

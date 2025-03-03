bfa9700d7198fcb0e70d0d733dee046a05c359f9
george-hall-ucl <george.hall@ucl.ac.uk>
2025-03-03 17:24:55 +0000

    Add tests for ADA and PDA

--- tests/testthat/test_run_dawnn.R
+++ tests/testthat/test_run_dawnn.R
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 University College London
+# Copyright (C) 2023-2025 University College London
 # Licensed under GNU GPL Version 3 <https://www.gnu.org/licenses/gpl-3.0.html>
 
 test_that("run_dawnn reproducible recalculate_graph = FALSE", {
@@ -60,4 +60,46 @@ test_that("run_dawnn fails if too few cells", {
                               reduced_dim = "pca", recalculate_graph = FALSE,
                               alpha = 0.1, verbosity = 0)),
                  "Dawnn requires at least 1001 cells. Your dataset contains 1000.")
+})
+
+test_that("run_dawnn different results if different da_mode", {
+    # Note that the labels are distributed as follows:
+    #   Condition1 Condition2
+    #          595        605
+    # run_dawnn with da_mode set to "ada" should therefore return a different
+    # object to if da_mode is "pda" (since the p-values should be different).
+    cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
+    cells <- FindNeighbors(cells, reduction = "pca", k.param = 1001,
+                           dims = 1:2, return.neighbor = TRUE)
+
+    dawnn_out_1 <- sm(run_dawnn(cells = cells, label_names = "label",
+                                label_1 = "Condition1", label_2 = "Condition2",
+                                reduced_dim = "pca", recalculate_graph = FALSE,
+                                alpha = 0.1, verbosity = 0, da_mode = "ada"))
+    dawnn_out_2 <- sm(run_dawnn(cells = cells, label_names = "label",
+                                label_1 = "Condition1", label_2 = "Condition2",
+                                reduced_dim = "pca", recalculate_graph = FALSE,
+                                alpha = 0.1, verbosity = 0, da_mode = "pda"))
+
+    expect_failure(expect_equal(dawnn_out_1, dawnn_out_2))
+})
+
+test_that("run_dawnn same results if different da_mode but exactly even labels", {
+    cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
+    # Make exacetly 600 labels with each condition:
+    cells$label[which(cells$label == "Condition2")[1:5]] <- "Condition1"
+
+    cells <- FindNeighbors(cells, reduction = "pca", k.param = 1001,
+                           dims = 1:2, return.neighbor = TRUE)
+
+    dawnn_out_1 <- sm(run_dawnn(cells = cells, label_names = "label",
+                                label_1 = "Condition1", label_2 = "Condition2",
+                                reduced_dim = "pca", recalculate_graph = FALSE,
+                                alpha = 0.1, verbosity = 0, da_mode = "ada"))
+    dawnn_out_2 <- sm(run_dawnn(cells = cells, label_names = "label",
+                                label_1 = "Condition1", label_2 = "Condition2",
+                                reduced_dim = "pca", recalculate_graph = FALSE,
+                                alpha = 0.1, verbosity = 0, da_mode = "pda"))
+
+    expect_equal(dawnn_out_1, dawnn_out_2)
 })

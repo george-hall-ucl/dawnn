c48d5033b787a6f0328758e8912c5f34e366364b
george-hall-ucl <george.hall@ucl.ac.uk>
2026-07-14 11:26:14 +0100

    Fix conda env changing tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -5,3 +5,13 @@ library(withr)
 library(Seurat)
 
 sm <- suppressMessages
+sep_r <- function(x, print_stdout = TRUE, print_stderr = FALSE) {
+    res <- callr::r(x, stdout = "/tmp/out", stderr = "/tmp/err")
+    if (print_stdout) {
+        print(readLines("/tmp/out"))
+    }
+    if (print_stderr) {
+        print(readLines("/tmp/err"))
+    }
+    return(res)
+}
--- tests/testthat/test_conda_env_changing.R
+++ tests/testthat/test_conda_env_changing.R
@@ -1,41 +1,41 @@
 test_that("Can change conda env to one containing TensorFlow", {
-    res_list <- list()
-    for (tf_conda_env_val in c("none_set", "tf_env")) {
+    result <- sep_r(function() {
+        devtools::load_all("../..")
         cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
-        cells <- FindNeighbors(cells, reduction = "pca", k.param = 1001,
-                               dims = 1:2, return.neighbor = TRUE)
-        if (tf_conda_env_val == "none_set") {
-            dawnn_out <- sm(run_dawnn(cells = cells, label_names = "label",
-                                      label_1 = "Condition1", label_2 = "Condition2",
-                                      reduced_dim = "pca", recalculate_graph = FALSE,
-                                      alpha = 0.1, verbosity = 0))
-        } else if (tf_conda_env_val == "tf_env") {
-            dawnn_out <- sm(run_dawnn(cells = cells, label_names = "label",
-                                      label_1 = "Condition1", label_2 = "Condition2",
-                                                reduced_dim = "pca", recalculate_graph = FALSE,
-                                                alpha = 0.1, verbosity = 0, tf_conda_env = "tf_env"))
-        }
-        res_list[[tf_conda_env_val]] <- dawnn_out
-    }
-    expect_equal(res_list[["none_set"]], res_list[["tf_env"]])
+        cells <- Seurat::FindNeighbors(cells, reduction = "pca", k.param = 1001,
+                                       dims = 1:2, return.neighbor = TRUE)
+        dawnn::run_dawnn(cells = cells, label_names = "label", label_1 = "Condition1",
+                         label_2 = "Condition2", reduced_dim = "pca",
+                         recalculate_graph = FALSE, alpha = 0.1, verbosity = 0,
+                         tf_conda_env = "tf_env")
+    })
+    expect_equal(sum(result$dawnn_p_vals), 269.095362)
+})
+
+
+test_that("Crashes if no conda env specified and correct packages not installed in base", {
+    result <- try(sep_r(function() {
+        devtools::load_all("../..")
+        cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
+        cells <- Seurat::FindNeighbors(cells, reduction = "pca", k.param = 1001,
+                                       dims = 1:2, return.neighbor = TRUE)
+        dawnn::run_dawnn(cells = cells, label_names = "label", label_1 = "Condition1",
+                         label_2 = "Condition2", reduced_dim = "pca",
+                         recalculate_graph = FALSE, alpha = 0.1, verbosity = 0)
+    }, print_stdout = FALSE))
+    expect_s3_class(result, "try-error")
 })
 
 test_that("Dawnn crashes if change conda env to one without TensorFlow", {
-    m <- paste("Tensorflow not installed in reticulate environment\\\\?.",
-               "Please install following",
-               "rstudio\\\\?.github\\\\?.io/reticulate/articles/python_packages\\\\?.html\\\\?.")
-
-    res <- try(callr::r(function() {
+    result <- try(sep_r(function() {
         devtools::load_all("../..")
         cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
-        cells <- Seurat::FindNeighbors(cells, reduction = "pca",
-                                       k.param = 1001, dims = 1:2,
-                                       return.neighbor = TRUE)
-        dawnn::run_dawnn(cells = cells, label_names = "label",
-                         label_1 = "Condition1", label_2 = "Condition2",
-                         reduced_dim = "pca", recalculate_graph = FALSE,
-                         alpha = 0.1, verbosity = 0, tf_conda_env = "base")}))
-
-    expect_match(res[[1]], m)
-    expect_s3_class(res, "try-error")
+        cells <- Seurat::FindNeighbors(cells, reduction = "pca", k.param = 1001,
+                                       dims = 1:2, return.neighbor = TRUE)
+        dawnn::run_dawnn(cells = cells, label_names = "label", label_1 = "Condition1",
+                         label_2 = "Condition2", reduced_dim = "pca",
+                         recalculate_graph = FALSE, alpha = 0.1, verbosity = 0,
+                         tf_conda_env = "base")
+    }, print_stdout = FALSE))
+    expect_s3_class(result, "try-error")
 })

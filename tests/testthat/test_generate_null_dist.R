5725531a0004e57e73d59bae7caaa54157bbcaa3
george-hall-ucl <george.hall@ucl.ac.uk>
2026-07-15 14:38:37 +0100

    Run null dist tests inside callr environment

--- tests/testthat/test_generate_null_dist.R
+++ tests/testthat/test_generate_null_dist.R
@@ -2,17 +2,21 @@
 # Licensed under GNU GPL Version 3 <https://www.gnu.org/licenses/gpl-3.0.html>
 
 test_that("generate_null_distribution returns vector of correct length", {
-    cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
-    set.seed(123)
-    cells <- FindNeighbors(cells, reduction = "pca", k.param = 1001,
-                           dims = 1:2, return.neighbor = TRUE)
-
-
-    model_file <- "~/.dawnn/dawnn_nn_model.h5"
-    model <- load_model_from_python(model_file)
-    out <- sm(generate_null_dist(cells, model, label_names = "label",
-                                 label_1 = "Condition1",
-                                 label_2 = "Condition2",
-                                 verbosity = 1))
-    expect_vector(out, ptype = double(), size = 3600)
+              out <- sep_r(function() {
+                        reticulate::use_condaenv("tf_env")
+                        cells <- readRDS("../data/dawnn_test_data_1200_cells_discrete_clusters_1gene_2pc.rds")
+                        set.seed(123)
+                        cells <- Seurat::FindNeighbors(cells, reduction = "pca",
+                                                       k.param = 1001, dims = 1:2,
+                                                       return.neighbor = TRUE)
+                        model_file <- "~/.dawnn/dawnn_nn_model.h5"
+                        model <- dawnn:::load_model_from_python(model_file)
+                        out <- suppressMessages(dawnn:::generate_null_dist(cells, model,
+                                                                          label_names = "label",
+                                                                          label_1 = "Condition1",
+                                                                          label_2 = "Condition2",
+                                                                          verbosity = 1,
+                                                                          da_mode = "ada"))
+                })
+              expect_vector(out, ptype = double(), size = 3600)
 })
